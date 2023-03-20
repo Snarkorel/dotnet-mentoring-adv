34b454e98f2025161b1a49fba9a2ce548a7a5026
Sergei Piontkovskii <Sergei_Piontkovskii@epam.com>
2023-03-20 21:34:00 +0100

    [Module 2] added CatalogService entity stubs

---
+++ Module 2/CatalogService/Entities/Category.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CatalogService.Entities
+{
+    public class Category
+    {
+        [Required]
+        public string Name { get; set; }
+
+        public string Image { get; set; }
+
+        public Category ParentCategory { get; set; }
+    }
+}
---
+++ Module 2/CatalogService/Entities/Product.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CatalogService.Entities
+{
+    public class Product
+    {
+        [Required]
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string Image { get; set; }
+
+        [Required]
+        public Category Category { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+
+        public uint Amount { get; set; }
+    }
+}
