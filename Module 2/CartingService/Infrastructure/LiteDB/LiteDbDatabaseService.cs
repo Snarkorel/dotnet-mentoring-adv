df63acfb31ac8cc4bfa94e3efe206bd2a086e02f
Sergei Piontkovskii <Sergei_Piontkovskii@epam.com>
2023-03-22 22:33:41 +0100

    [Module 2] LiteDbDatabaseService: changed case of "DB" in class name to "Db"

--- Module 2/CartingService/Infrastructure/LiteDB/LiteDbDatabaseService.cs
+++ Module 2/CartingService/Infrastructure/LiteDB/LiteDbDatabaseService.cs
@@ -4,14 +4,14 @@ using System.Reflection;
 
 namespace CartingService.Infrastructure.LiteDB
 {
-    internal class LiteDBDatabaseService<TEntity> : IDatabaseService<TEntity>
+    internal class LiteDbDatabaseService<TEntity> : IDatabaseService<TEntity>
     {
         private readonly ILiteDatabase _database;
         //Let's imagine that we're taking this string from the config
         private static readonly string _connectionString = $"filename={Assembly.GetExecutingAssembly().Location}.LiteDb.db;journal=false";
         private readonly ILiteCollection<TEntity> _collection;
 
-        internal LiteDBDatabaseService()
+        internal LiteDbDatabaseService()
         {
             _database = new LiteDatabase(_connectionString);
             _collection = _database.GetCollection<TEntity>();
