170efcf7de56b42a585ea53845978b199125274b
Sergei Piontkovskii <Sergei_Piontkovskii@epam.com>
2023-03-22 21:08:25 +0100

    [Module 2] CartingService: added Infrastructure service draft

---
+++ Module 2/CartingService/Infrastructure/IDatabaseService.cs
@@ -0,0 +1,10 @@
+﻿namespace CartingService.Infrastructure
+{
+    internal interface IDatabaseService<TEntity>
+    {
+        IEnumerable<TEntity> GetAll();
+        void Add(TEntity entity);
+        void Update(TEntity entity);
+        void Delete(int id);
+    }
+}
---
+++ Module 2/CartingService/Infrastructure/LiteDB/LiteDbDatabaseService.cs
@@ -0,0 +1,38 @@
+﻿using LiteDB;
+
+namespace CartingService.Infrastructure.LiteDB
+{
+    internal class LiteDBDatabaseService<TEntity> : IDatabaseService<TEntity>
+    {
+        private ILiteDatabase _database;
+        private readonly ILiteCollection<TEntity> _collection;
+
+        internal LiteDBDatabaseService(string connectionString)
+        {
+            //TODO: initialize db, check if db file exists, otherwise - create it
+            _database = new LiteDatabase(connectionString);
+
+            _collection = _database.GetCollection<TEntity>();
+        }
+
+        public IEnumerable<TEntity> GetAll()
+        {
+            return _collection.FindAll().ToList();
+        }
+        
+        public void Add(TEntity entity)
+        {
+            _collection.Insert(entity);
+        }
+
+        public void Update(TEntity entity)
+        {
+            _collection.Update(entity);
+        }
+
+        public void Delete(int id)
+        {
+            _collection.Delete(id);
+        }
+    }
+}
