541ba7a3c5776c9a9609bcc29f26711ebd13964f
Sergei Piontkovskii <Sergei_Piontkovskii@epam.com>
2023-03-22 22:14:06 +0100

    [Module 2] CartingService: added some business logic to CartingService Core

--- Module 2/CartingService/Core/CartingService.cs
+++ Module 2/CartingService/Core/CartingService.cs
@@ -1,11 +1,19 @@
 ﻿using CartingService.Core.Entities;
 using CartingService.Core.Interfaces;
+using CartingService.Infrastructure.Interfaces;
 
 namespace CartingService.Core
 {
     public class CartingService : ICartingService
     {
         private Cart _cart;
+        private readonly ICartItemRepository _repository;
+        
+        //TODO: logging, exception processing (out of task's scope)
+        private CartingService(ICartItemRepository repository)
+        {
+            _repository = repository;
+        }
 
         public CartingService(int cartId)
         {
@@ -14,17 +22,40 @@ namespace CartingService.Core
         
         public IEnumerable<CartItem> GetItems()
         {
-            throw new NotImplementedException();
+            return _repository.GetItems();
         }
 
         public bool AddItem(CartItem item)
         {
-            throw new NotImplementedException();
+            if (ItemExists(item.Id))
+                IncreaseCountIfExists(item);
+            else
+                _repository.Add(item);
+
+            return true;
         }
 
         public bool Remove(int itemId)
         {
-            throw new NotImplementedException();
+            if (!ItemExists(itemId))
+                return false;
+
+            _repository.Delete(itemId);
+            return true;
         }
+
+        private bool ItemExists(int id)
+        {
+            return GetItems().Any(x => x.Id == id);
+        }
+
+        private void IncreaseCountIfExists(CartItem item)
+        {
+            var existingItem = GetItems().First(x => x.Id == item.Id);
+            existingItem.Quantity += item.Quantity;
+            _repository.Update(existingItem);
+        }
+
+        
     }
 }
