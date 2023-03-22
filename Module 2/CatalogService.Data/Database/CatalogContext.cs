64f2a76e661450cdb797d229e2882acc7018b866
Sergei Piontkovskii <Sergei_Piontkovskii@epam.com>
2023-03-23 00:11:30 +0100

    [Module 2] CatalogService: added EFCore and DBcontext draft

---
+++ Module 2/CatalogService.Data/Database/CatalogContext.cs
@@ -0,0 +1,16 @@
+﻿using CatalogService.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CatalogService.Data.Database
+{
+    public class CatalogContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=.\SQLEXPRESS;Database=CatalogServiceDB;Trusted_Connection=True;");
+        }
+    }
+}
