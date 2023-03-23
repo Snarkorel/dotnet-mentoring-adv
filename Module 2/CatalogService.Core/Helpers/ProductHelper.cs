d69ce7c568d905e4da441f132e35fda9918352e1
Sergei Piontkovskii <Sergei_Piontkovskii@epam.com>
2023-03-23 03:28:38 +0100

    [Module 2] CatalogService.Core: added ProductHelper static class

---
+++ Module 2/CatalogService.Core/Helpers/ProductHelper.cs
@@ -0,0 +1,47 @@
+﻿using CatalogService.Core.Entities;
+using CatalogService.Data.Models;
+
+namespace CatalogService.Core.Helpers
+{
+    public static class ProductHelper
+    {
+        public static IQueryable<ProductItem> ProductsToProductItems(this IQueryable<Product> products)
+        {
+            return products.Select(x => new ProductItem
+            {
+                Name = x.Name,
+                Description = string.IsNullOrEmpty(x.Description) ? null : x.Description,
+                Image = x.Image == null ? null : new Uri(x.Image),
+                Category = CategoryHelper.CategoryToCategoryItem(x.Category),
+                Price = x.Price,
+                Amount = x.Amount
+            });
+        }
+
+        public static ProductItem ProductToProductItem(Product product)
+        {
+            return new ProductItem
+            {
+                Name = product.Name,
+                Description = string.IsNullOrEmpty(product.Description) ? null : product.Description,
+                Image = product.Image == null ? null : new Uri(product.Image),
+                Category = CategoryHelper.CategoryToCategoryItem(product.Category),
+                Price = product.Price,
+                Amount = product.Amount
+            };
+        }
+
+        public static Product ProductItemToProduct(ProductItem item)
+        {
+            return new Product
+            {
+                Name = item.Name,
+                Description = string.IsNullOrEmpty(item.Description) ? null : item.Description,
+                Image = item.Image == null ? null : item.Image.ToString(),
+                Category = CategoryHelper.CategoryItemToCategory(item.Category),
+                Price = item.Price,
+                Amount = item.Amount
+            };
+        }
+    }
+}
