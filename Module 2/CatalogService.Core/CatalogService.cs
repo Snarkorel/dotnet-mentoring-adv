f1a18c52dcf199f5f9fa03b875b1454c3b85743e
Sergei Piontkovskii <Sergei_Piontkovskii@epam.com>
2023-03-23 01:25:46 +0100

    [Module 2] CatalogService.Core: added ICatalogService method stubs

--- Module 2/CatalogService.Core/CatalogService.cs
+++ Module 2/CatalogService.Core/CatalogService.cs
@@ -1,9 +1,68 @@
 ﻿using CatalogService.Core.Interfaces;
+using CatalogService.Data.Interfaces;
+using CatalogService.Data.Models;
 
 namespace CatalogService.Core
 {
     public class CatalogService : ICatalogService
     {
+        private readonly IProductRepository _productRepository;
+        private readonly ICategoryRepository _categoryRepository;
 
+        public CatalogService(IProductRepository productRepository, ICategoryRepository categoryRepository)
+        {
+            _productRepository = productRepository;
+            _categoryRepository = categoryRepository;
+        }
+
+        public Category GetCategory(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<Category> ListCategories()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool AddCategory(Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateCategory(Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteCategory(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product GetProduct(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<Product> ListProducts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool AddProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteProduct(int id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- Module 2/CatalogService.Core/Interfaces/ICatalogService.cs
+++ Module 2/CatalogService.Core/Interfaces/ICatalogService.cs
@@ -1,7 +1,27 @@
-﻿namespace CatalogService.Core.Interfaces
+﻿using CatalogService.Data.Models;
+
+namespace CatalogService.Core.Interfaces
 {
     public interface ICatalogService
     {
+        Category GetCategory(int id);
 
+        IQueryable<Category> ListCategories();
+
+        bool AddCategory(Category category);
+
+        bool UpdateCategory(Category category);
+
+        bool DeleteCategory(int id);
+
+        Product GetProduct(int id);
+
+        IQueryable<Product> ListProducts();
+
+        bool AddProduct(Product product);
+
+        bool UpdateProduct(Product product);
+
+        bool DeleteProduct(int id);
     }
 }
