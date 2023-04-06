ca45977c1ea291c17091296b5c7ed64613c03e3f
Sergei Piontkovskii <Sergei_Piontkovskii@epam.com>
2023-04-06 13:14:00 +0200

    CartingService.WebAPI: implemented API methods (GetCart, AddItem, DeleteItem)

--- DotnetMntrngAdv/CartingService.WebApi/Program.cs
+++ DotnetMntrngAdv/CartingService.WebApi/Program.cs
@@ -21,7 +21,7 @@ namespace CartingService.WebApi
 
             app.MapGet("/cart/{key}", GetCartInfo);
             //TODO: add versioning support
-            //app.MapGet("/cart/{key}", GetCartItems);
+            app.MapGet("/cart/{key}/TEMP", GetCartItems); //TODO
 
             app.MapPost("/cart/{key}", AddItem);
             app.MapDelete("/cart/{key}/{id}", DeleteItem);
@@ -32,27 +32,55 @@ namespace CartingService.WebApi
         //v1 method of GET /cart/{key}
         private static async Task<IResult> GetCartInfo(ICartingService service, string key)
         {
-            //TODO
-            throw new NotImplementedException();
+            try
+            {
+                var cart = await service.GetCartInfo(key);
+                return TypedResults.Ok(cart);
+            }
+            catch (ArgumentException)
+            {
+                return TypedResults.NotFound();
+            }
         }
 
         //v2 method of GET /cart/{key}
         private static async Task<IResult> GetCartItems(ICartingService service, string key)
         {
-            //TODO
-            throw new NotImplementedException();
+            try
+            {
+                var cart = await service.GetCartInfo(key);
+                return TypedResults.Ok(cart.Items);
+            }
+            catch (ArgumentException)
+            {
+                return TypedResults.NotFound();
+            }
         }
 
         private static async Task<IResult> AddItem(ICartingService service, string key, [FromBody] CartItem item)
         {
-            //TODO
-            throw new NotImplementedException();
+            try
+            {
+                await service.AddItem(key, item);
+                return TypedResults.Ok();
+            }
+            catch (Exception ex) //Note: exception details exposed for debugging reasons
+            {
+                return TypedResults.Problem(new ProblemDetails { Detail = ex.StackTrace, Title = ex.Message, Status = 500 });
+            }
         }
 
         private static async Task<IResult> DeleteItem(ICartingService service, string key, int id)
         {
-            //TODO
-            throw new NotImplementedException();
+            try
+            {
+                await service.RemoveItem(key, id);
+                return TypedResults.Ok();
+            }
+            catch (Exception ex)
+            {
+                return TypedResults.Problem(new ProblemDetails { Detail = ex.StackTrace, Title = ex.Message, Status = 500 });
+            }
         }
     }
 }
