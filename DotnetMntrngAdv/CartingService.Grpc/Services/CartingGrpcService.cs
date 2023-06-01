0ffdd4b73e3c62f374a6ca94e81275fd3353a8ae
Sergei Piontkovskii <Sergei_Piontkovskii@epam.com>
2023-06-01 22:08:57 +0200

    CartingService.Grpc: Service methods basically implemented (not tested yet), updated carting.proto (fixed issue with wrong response types on Add methods)

--- DotnetMntrngAdv/CartingService.Grpc/Services/CartingGrpcService.cs
+++ DotnetMntrngAdv/CartingService.Grpc/Services/CartingGrpcService.cs
@@ -1,7 +1,5 @@
-using CartingService.Grpc;
 using Grpc.Core;
 using System.Globalization;
-using System.IO;
 using CartingService.Core.Entities;
 using CartingService.Core.Interfaces;
 using Google.Protobuf.Collections;
@@ -18,22 +16,13 @@ namespace CartingService.Grpc.Services
             _cartingService = cartingService;
         }
 
-        //public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
-        //{
-        //    return Task.FromResult(new HelloReply
-        //    {
-        //        Message = "Hello " + request.Name
-        //    });
-        //}
+        //TODO: logging (also setup loglevel in configs or startup class), cancellation tokens
+        //TODO: handle service exceptions
 
-        //TODO!
-
+        //Unary call
         public override async Task<GetItemsReply> GetItems(GetItemsRequest request, ServerCallContext context)
         {
-            var cart = await _cartingService.GetCartInfo(request.CartName);
-            var items = cart.Items;
-
-            var convertedItems = ConvertItems(items);
+            var convertedItems = await GetAndConvertCartItems(request.CartName);
 
             var response = new GetItemsReply();
             response.Item.AddRange(convertedItems);
@@ -41,19 +30,78 @@ namespace CartingService.Grpc.Services
             return response;
         }
 
-        public override Task GetItemsStream(GetItemsRequest request, IServerStreamWriter<GetItemsReply> responseStream, ServerCallContext context)
+        //Server streaming
+        public override async Task GetItemsStream(GetItemsRequest request, IServerStreamWriter<GetItemsReply> responseStream, ServerCallContext context)
         {
-            return base.GetItemsStream(request, responseStream, context);
+            var items = await GetCartItems(request.CartName);
+
+            foreach (var item in items)
+            {
+                if (context.CancellationToken.IsCancellationRequested)
+                    return;
+
+                var response = new GetItemsReply();
+                response.Item.Add(ConvertToItem(item));
+                await responseStream.WriteAsync(response);
+                await Task.Delay(TimeSpan.FromSeconds(1), context.CancellationToken);
+            }
+        }
+        
+        //Client streaming
+        public override async Task<AddItemReply> AddItem(IAsyncStreamReader<AddItemRequest> requestStream, ServerCallContext context)
+        {
+            var cartName = string.Empty;
+            while (await requestStream.MoveNext())
+            {
+                var message = requestStream.Current;
+
+                if (cartName == string.Empty)
+                    cartName = message.CartName;
+
+                var requestItems = message.Item;
+                foreach (var item in requestItems)
+                {
+                    var cartItem = ConvertToCartItem(item);
+                    await _cartingService.AddItem(cartName, cartItem);
+                }
+                
+            }
+
+            var items = await GetAndConvertCartItems(cartName);
+            var response = new AddItemReply();
+            response.Item.AddRange(items);
+
+            return response;
         }
 
-        public override Task<AddItemRequest> AddItem(IAsyncStreamReader<AddItemRequest> requestStream, ServerCallContext context)
-        {
-            return base.AddItem(requestStream, context);
-        }
 
-        public override Task AddItemStream(IAsyncStreamReader<AddItemRequest> requestStream, IServerStreamWriter<AddItemRequest> responseStream, ServerCallContext context)
+        //Bidirectional streaming
+        public override async Task AddItemStream(IAsyncStreamReader<AddItemRequest> requestStream, IServerStreamWriter<AddItemReply> responseStream, ServerCallContext context)
         {
-            return base.AddItemStream(requestStream, responseStream, context);
+            var cartName = string.Empty;
+            
+            var readTask = Task.Run(async () =>
+            {
+                await foreach (var message in requestStream.ReadAllAsync())
+                {
+                    foreach (var item in message.Item)
+                    {
+                        if (cartName == string.Empty)
+                            cartName = message.CartName;
+                        await _cartingService.AddItem(message.CartName, ConvertToCartItem(item));
+                    }
+                }
+            });
+            
+            while (!readTask.IsCompleted)
+            {
+                var items = await GetAndConvertCartItems(cartName);
+                var response = new AddItemReply();
+                response.Item.AddRange(items);
+
+                await responseStream.WriteAsync(response);
+                await Task.Delay(TimeSpan.FromSeconds(1), context.CancellationToken);
+            }
         }
 
         private static decimal GetDecimal(string value)
@@ -61,22 +109,51 @@ namespace CartingService.Grpc.Services
             return decimal.Parse(value, CultureInfo.InvariantCulture);
         }
 
-        private RepeatedField<Item> ConvertItems(IEnumerable<CartItem> items)
+        private static RepeatedField<Item> ConvertItems(IEnumerable<CartItem> items)
         {
             var convertedItems = new RepeatedField<Item>();
             foreach (var item in items)
             {
-                convertedItems.Add(new Item
-                {
-                    Id = item.Id,
-                    Image = item.Image.ToString(),
-                    Name = item.Name,
-                    Price = item.Price.ToString(CultureInfo.InvariantCulture),
-                    Quantity = item.Quantity,
-                });
+                convertedItems.Add(ConvertToItem(item));
             }
 
             return convertedItems;
+        }
+
+        private static Item ConvertToItem(CartItem item)
+        {
+            return new Item
+            {
+                Id = item.Id,
+                Image = item.Image.ToString(),
+                Name = item.Name,
+                Price = item.Price.ToString(CultureInfo.InvariantCulture),
+                Quantity = item.Quantity,
+            };
+        }
+
+        private static CartItem ConvertToCartItem(Item item)
+        {
+            return new CartItem
+            {
+                Id = item.Id,
+                Image = new Uri(item.Image),
+                Name = item.Name,
+                Price = GetDecimal(item.Price),
+                Quantity = item.Quantity
+            };
+        }
+
+        private async Task<IEnumerable<CartItem>> GetCartItems(string cartName)
+        {
+            var cart = await _cartingService.GetCartInfo(cartName);
+            return cart.Items;
+        }
+
+        private async Task<RepeatedField<Item>> GetAndConvertCartItems(string cartName)
+        {
+            var items = await GetCartItems(cartName);
+            return ConvertItems(items);
         }
     }
 }
