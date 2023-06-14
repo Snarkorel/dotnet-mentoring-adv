0cd396ad5a324df524b49f461661790d3347df7f
Sergei Piontkovskii <Sergei_Piontkovskii@epam.com>
2023-06-14 13:03:19 +0200

    CartingService: added logging

--- DotnetMntrngAdv/CartingService/Core/CartingService.cs
+++ DotnetMntrngAdv/CartingService/Core/CartingService.cs
@@ -2,6 +2,7 @@
 using CartingService.Core.Interfaces;
 using Infrastructure.ServiceBus.DTO;
 using Infrastructure.ServiceBus.Interfaces;
+using Microsoft.Extensions.Logging;
 
 namespace CartingService.Core
 {
@@ -9,26 +10,31 @@ namespace CartingService.Core
     {
         private readonly ICartRepository _cartRepository;
         private readonly IMessageListener _messageListener;
+        private readonly ILogger<ICartingService> _logger;
 
-        public CartingService(ICartRepository cartRepository, IMessageListener messageListener)
+        public CartingService(ICartRepository cartRepository, IMessageListener messageListener, ILogger<ICartingService> logger)
         {
             _cartRepository = cartRepository;
+            _logger = logger;
             _messageListener = messageListener;
             _messageListener.Subscribe(ProcessItemChanges);
         }
 
         public async Task<Cart> GetCartInfo(string key)
         {
+            _logger.LogInformation($"Getting cart info for cart '{key}'");
             return await Task.FromResult(_cartRepository.GetCart(key));
         }
 
         public async Task AddItem(string key, CartItem item)
         {
+            _logger.LogInformation($"Adding a new item [id={item.Id} name={item.Name} price={item.Price} quantity={item.Quantity}] to cart '{key}'");
             await Task.Run(() => _cartRepository.AddItem(key, item));
         }
 
         public async Task RemoveItem(string key, int itemId)
         {
+            _logger.LogInformation($"Removing item id={itemId} from cart '{key}'");
             await Task.Run(() => _cartRepository.Delete(key, itemId));
         }
 
